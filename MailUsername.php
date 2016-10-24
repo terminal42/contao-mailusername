f2ed4d92ae5650a822b977adfb5f3ac6a891432c
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-10-24 11:00:54 +0200

    Updated copyright statement

--- MailUsername.php
+++ MailUsername.php
@@ -3,7 +3,7 @@
 /**
  * mailusername extension for Contao Open Source CMS
  *
- * @copyright  Copyright (c) 2009-2015, terminal42 gmbh
+ * @copyright  Copyright (c) 2009-2016, terminal42 gmbh
  * @author     terminal42 gmbh <info@terminal42.ch>
  * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
  * @link       http://github.com/terminal42/contao-mailusername
