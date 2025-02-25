4b788a6dcc3241293b2a4df5dc8e902d736fd792
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-02-25 09:10:14 +0100

    Allow 255 chars for email and username

--- contao/dca/tl_member.php
+++ contao/dca/tl_member.php
@@ -1,8 +1,7 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_member']['fields']['email']['eval']['unique'] = true;
-$GLOBALS['TL_DCA']['tl_member']['fields']['email']['eval']['maxlength'] = 64;
 $GLOBALS['TL_DCA']['tl_member']['fields']['username']['eval']['rgxp'] = 'email';
 $GLOBALS['TL_DCA']['tl_member']['fields']['username']['eval']['disabled'] = true;
 $GLOBALS['TL_DCA']['tl_member']['fields']['username']['eval']['mandatory'] = false;
-$GLOBALS['TL_DCA']['tl_member']['fields']['username']['sql'] = "varchar(64) COLLATE utf8mb4_unicode_ci NULL";
+$GLOBALS['TL_DCA']['tl_member']['fields']['username']['sql'] = 'varchar(255) COLLATE utf8mb4_unicode_ci NULL';
